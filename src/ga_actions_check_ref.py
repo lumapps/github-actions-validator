8119cc78b2903910806cfdf7dc908d87045149b5
prousso <pascal@lumapps.com>
2021-05-24 19:05:55 +0200

    feat: create actions

--- src/ga_actions_check_ref.py
+++ src/ga_actions_check_ref.py
@@ -2,37 +2,42 @@
 import glob
 import re
 import sys
-import yaml
 from typing import IO, Mapping
 
+import yaml
+
+ALLOWED_ORGANIZATIONS = ["actions", "lumapps"]
 SHORT_SHA1_PATTERN = re.compile(
     r"^(?P<actions_name>[A-Za-z0-9_.-]*\/[A-Za-z0-9_.-]*)@(?P<actions_version>[a-f0-9]{7})$"
 )
 
 
+# pylint: disable=too-many-nested-blocks,too-many-branches
 def is_github_workflow_valid(file: IO, allowed_actions: Mapping[str, str]) -> bool:
     is_valid = True
-    document = yaml.safe_load(file)
-    for environment in document["jobs"].values():
+    for environment in yaml.safe_load(file)["jobs"].values():
         for step in environment["steps"]:
             actions = step.get("uses", "")
-            if actions and not actions.startswith("actions/"):
+            organization = actions.split("/")[0] if actions else None
+            if organization and organization not in ALLOWED_ORGANIZATIONS:
                 if not re.match(SHORT_SHA1_PATTERN, actions):
                     print(
-                        f"ERROR: Actions {actions} must follow "
-                        f"'actions_name@sohrt_sha1_tag  # github_tag' format."
+                        f"ERROR {file.name}: Actions {actions} must follow "
+                        f"'actions_name@short_sha1_tag  # github_tag' format."
                     )
                     is_valid = False
                 else:
-                    match = re.match(SHORT_SHA1_PATTERN, actions).groupdict()
-                    actions_name = match["actions_name"]
-                    actions_version = match["actions_version"]
+                    match = re.match(SHORT_SHA1_PATTERN, actions).groupdict()  # type: ignore
+                    actions_name, actions_version = (
+                        match["actions_name"],
+                        match["actions_version"],
+                    )
                     if actions_name not in allowed_actions.keys():
-                        print(f"ERROR: Actions {actions_name} forbidden")
+                        print(f"ERROR {file.name}: Actions {actions_name} forbidden")
                         is_valid = False
                     elif actions_version != allowed_actions[actions_name]:
                         print(
-                            f"ERROR: Version {actions_version} forbidden, "
+                            f"ERROR {file.name}: Version {actions_version} forbidden, "
                             f"instead you must use {allowed_actions[actions_name]}."
                         )
                         is_valid = False
@@ -42,7 +47,7 @@ def is_github_workflow_valid(file: IO, allowed_actions: Mapping[str, str]) -> bo
 
 
 def load_allowed_actions() -> Mapping[str, str]:
-    with open("ALLOWED_ACTIONS.yaml", mode="r") as file:
+    with open("./ALLOWED_ACTIONS.yaml", mode="r") as file:
         return yaml.safe_load(file)
 
 
